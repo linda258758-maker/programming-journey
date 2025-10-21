407c3001b42c98918a2ee20dac6889663e2e8172
linda258758-maker <linda258758@student.smkn1kandeman.sch.id>
2025-10-21 09:44:38 +0700

    Add files via upload

---
+++ praktik6.8_linda_pplg2/praktik6.8_linda_pplg2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik6._8_linda_pplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulangiPilihan;
+
+            do
+            {
+                // --- Blok Proses yang Akan Diulang ---
+                Console.WriteLine("--------------------------------");
+                Console.Write("Masukkan nama Anda: ");
+                string nama = Console.ReadLine();
+                Console.WriteLine($"Halo, {nama}! Proses telah selesai.");
+                //------------------------------------
+                // Pertanyaan untuk mengulang
+                Console.Write("Apakah Anda ingin mengulang lagi? (ya/tidak): ");
+                ulangiPilihan = Console.ReadLine().ToLower(); // Mengambil input dan mengubah ke huruf kecil
+
+                Console.WriteLine(); // Baris kosong untuk kerapian
+
+                // Kondisi diperiksa di akhir: Ulangi selama pengguna mengetik "ya"
+            } while (ulangiPilihan == "ya");
+              Console.WriteLine("Terima kasih. Program selesai.");
+            }
+    }
+}
