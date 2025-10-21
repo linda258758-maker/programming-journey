9ce34c9287dd05f49aef03fbf098df2c45ecfa98
linda258758-maker <linda258758@student.smkn1kandeman.sch.id>
2025-10-21 15:18:37 +0700

    Add files via upload

---
+++ praktik7.1/praktik7.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik7._1
+{
+    internal class Program
+    {
+        static void Sapa()
+        {
+            Console.WriteLine("Halo, selamat datang di pembeljaran C#!");
+            Console.WriteLine("Mari belajar tentang fungsi!");
+        }
+
+    static void Main(string[] args)
+        {
+            Sapa(); //Memanggil fungsi
+        }
+    }
+}
