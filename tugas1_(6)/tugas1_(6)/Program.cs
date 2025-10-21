e39bb3fb5082c7f2e7267467b32fdaaa04301abc
linda258758-maker <linda258758@student.smkn1kandeman.sch.id>
2025-10-21 09:45:23 +0700

    Add files via upload

---
+++ tugas1_(6)/tugas1_(6)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas1__6_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+
+            Console.WriteLine("Menghitung perkalian 5: ");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                total = total * i;
+                Console.WriteLine($"{5 * i}");
+
+            }
+        }
+    }
+}
