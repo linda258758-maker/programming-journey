3a320eb667ab9da39a7ad04988a0e796094d6ab3
linda258758-maker <linda258758@student.smkn1kandeman.sch.id>
2025-10-21 09:40:41 +0700

    Add files via upload

---
+++ praktik6.1_linda_pplg2/praktik6.1_linda_pplg2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik6._1_linda_pplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Perulangan untuk menampilkan angka 1 sampai 10:");
+
+            // Perulangan for dimulai disini 
+            for (int i = 1; i <= 10; i++)
+            {
+                // Blok kode yang akan diulang
+                Console.WriteLine("Angka ke-" + i);
+            }
+            Console.WriteLine("\nPerulangan selesai.");
+        }
+    }
+}
