2d05094dd8fd370473fd005df515e9dd5a68348c
linda258758-maker <linda258758@student.smkn1kandeman.sch.id>
2025-10-21 15:20:30 +0700

    Add files via upload

---
+++ tugas1_(7)/tugas1_(7)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas1__7_
+{
+    internal class Program
+    {
+        static double HitungKali(double angka1, double angka2, double angka3)
+        {
+            double hasil = angka1 * angka2 * angka3;
+            return hasil;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan Angka pertama: ");
+            double angka1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan Angka kedua: ");
+            double angka2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan Angka ketiga: ");
+            double angka3 = Convert.ToDouble(Console.ReadLine());
+
+            double hasil = HitungKali(angka1, angka2, angka3);
+            Console.WriteLine("Hasil perkalian = " + hasil);
+        }
+    }
+}
