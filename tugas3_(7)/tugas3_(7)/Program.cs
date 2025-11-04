a92f5a028a52d55e2bfaa4f4715d23e515453e1e
linda258758-maker <linda258758@student.smkn1kandeman.sch.id>
2025-11-04 09:10:21 +0700

    Add files via upload

---
+++ tugas3_(7)/tugas3_(7)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas3__7_
+{
+    internal class Program
+    {
+        static double KonversiSuhu(double c)
+        {
+            double f = (c * 9 / 5) + 32;
+            return f;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan suhu dalam Celcius: ");
+            double celcius = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheit = KonversiSuhu(celcius);
+
+            Console.WriteLine("Suhu dalam Fahrenheit adalah:  " + fahrenheit);
+        }
+    }
+}
