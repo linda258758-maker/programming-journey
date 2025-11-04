4f8de9b9295617408d8a4c42d07cc8d1e38a98e0
linda258758-maker <linda258758@student.smkn1kandeman.sch.id>
2025-11-04 09:09:22 +0700

    Add files via upload

---
+++ tugas2_(7)/tugas2_(7)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas2__7_
+{
+    internal class Program
+    {
+        static double RataRata(double nilai1, double nilai2, double nilai3)
+        {
+            double hasil = (nilai1 + nilai2 + nilai3) / 3;
+            return hasil;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menghitung Rata-rata nilai");
+
+            Console.Write("Masukkan nilai pertama: ");
+            double nilai1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Masukkan nilai kedua: ");
+            double nilai2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Masukkan nilai ketiga: ");
+            double nilai3 = double.Parse(Console.ReadLine());
+
+            double hasil = RataRata(nilai1, nilai2, nilai3);
+            Console.WriteLine("Rata-rata nilai = " + hasil);
+        }
+    }
+}
